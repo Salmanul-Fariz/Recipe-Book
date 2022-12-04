f15391bb529b75b0ceaeab3506cbaede566c8771
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-04 16:57:39 +0530

    adding a shopping list edit section

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {}
