ae25d8618cbe1cb3b68cd6f3a9a66efd9bec93e2
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-04 10:51:40 +0530

    adding content to the recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipes } from '../recipes.modal';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipes[] = [
+    new Recipes(
+      'Easy Recipie',
+      'Description of a recipe food',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'
+    ),
+  ];
 }
