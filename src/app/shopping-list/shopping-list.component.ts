0182d226d92efa700c0ab5e7a27d91b83cea496a
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-04 15:46:31 +0530

    created and outputting the shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { Ingrediant } from '../shared/ingrediants.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent {}
+export class ShoppingListComponent {
+  ingredients: Ingrediant[] = [
+    new Ingrediant('Apple', 10),
+    new Ingrediant('Orange', 5),
+    new Ingrediant('Tomato', 20),
+  ];
+}
