d81786cfec1e799e7b908dbd44289e190d9f43c7
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-13 11:49:48 +0530

    add ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingrediant } from '../shared/ingrediants.model';
 import { Recipes } from './recipes.modal';
 
 export class RecipeService {
@@ -8,12 +9,22 @@ export class RecipeService {
     new Recipes(
       'Easy Recipie',
       'Description of a recipe food',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+      [
+        new Ingrediant('Bread', 2),
+        new Ingrediant('Carrot', 4),
+        new Ingrediant('Apple', 5),
+      ]
     ),
     new Recipes(
       'medium Recipie',
       'Description of a recipe food',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+      [
+        new Ingrediant('Tomato', 10),
+        new Ingrediant('Onion', 4),
+        new Ingrediant('Apple', 5),
+      ]
     ),
   ];
 
--- src/app/recipes/recipes.modal.ts
+++ src/app/recipes/recipes.modal.ts
@@ -1,11 +1,20 @@
+import { Ingrediant } from '../shared/ingrediants.model';
+
 export class Recipes {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingrediant[];
 
-  constructor(name: string, description: string, imagepath: string) {
+  constructor(
+    name: string,
+    description: string,
+    imagepath: string,
+    ingredients: Ingrediant[]
+  ) {
     this.name = name;
     this.description = description;
     this.imagePath = imagepath;
+    this.ingredients = ingredients;
   }
 }
